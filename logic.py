e7cb345f71074d8f49837e6fb484f71f53ef7454
Andree Wijaya <andree@twiscode.com>
2017-05-30 21:50:54 +0700

    Adjusting PEP

--- logic.py
+++ logic.py
@@ -1,9 +1,10 @@
-#Serves as the controller to the model and database
+# Serves as the controller to the model and database
 
 import hmac
 import os
 import hashlib
-import time, datetime
+import time
+import datetime
 from urlparse import urlparse, urljoin
 from flask import request, url_for, render_template, redirect, jsonify
 from flask_login import login_user
@@ -20,11 +21,14 @@ dbsession = DBSession()
 file = open('static/secret.txt', 'r')
 SECRET = file.read()
 
+
 def hash_str(s):
     return hmac.new(SECRET, s, digestmod=hashlib.sha256).hexdigest()
 
+
 def get_date():
     return time.strftime("%d %b %Y")
+
 
 def is_safe_url(target):
     ref_url = urlparse(request.host_url)
@@ -32,38 +36,47 @@ def is_safe_url(target):
     return test_url.scheme in ('http', 'https') and \
            ref_url.netloc == test_url.netloc
 
+
 def get_all_posts():
     return dbsession.query(Post).all()
+
 
 def getAllPosts():
     posts = dbsession.query(Post).all()
     return jsonify(Posts=[i.serialize for i in posts])
 
+
 def getPostJson(post_id):
     post = get_post_byID(post_id)
     return jsonify(Post= post.serialize)
 
+
 def get_post_byID(post_id):
     return dbsession.query(Post).filter_by(id=post_id).one()
+
 
 def get_post_by_user(user):
     posts = dbsession.query(Post).filter_by(user=user).all()
     return posts
+
 
 def get_user(user_id):
     dbsession.rollback()
     user = dbsession.query(User).get(int(user_id))
     return user
 
+
 def get_user_from_email(email):
     user = dbsession.query(User).get(email)
     return user
+
 
 def create_city(city):
     newCity = City(name=city)
     dbsession.add(newCity)
     dbsession.commit()
     return newCity
+
 
 def check_city(city):
     find_city = dbsession.query(City).filter_by(name=city).one()
@@ -75,6 +88,7 @@ def check_city(city):
     else:
         newCity = create_city(city)
         return newCity
+
 
 def create_new_post(name, age, tod, resting_at, burried_at, burried_date, picture, obituary, user, city):
     todayDate = datetime.date.today()
@@ -92,6 +106,7 @@ def create_new_post(name, age, tod, resting_at, burried_at, burried_date, pictur
     dbsession.commit()
     return newPost
 
+
 def update_post(post_id, name, age, tod, resting_at, burried_at, burried_date, picture, obituary, user, city):
     post = get_post_byID(post_id)
     post.d_name = name
@@ -106,16 +121,18 @@ def update_post(post_id, name, age, tod, resting_at, burried_at, burried_date, p
     dbsession.commit()
     return post
 
+
 def delete_post(post_id):
     post = get_post_byID(post_id)
     dbsession.commit()
     dbsession.delete(post)
     dbsession.commit()
 
+
 def process_login(email, password, remember):
         if email and password:
             user = validate_email(email)
-            print "User received ? %s" %user.id
+            print "User received ? %s" % user.id
             if user:
                 if check_password_valid(user, password):
                     proceed_to_login(user, remember)
@@ -128,17 +145,18 @@ def process_login(email, password, remember):
         else:
             return show_error('Please fill in both email & password!', 'login.html')
 
+
 def register_user(name, email, password):
         error = ""
         checkUser = validate_email(email)
 
         if checkUser:
             error = "Email has been used, please use other email"
-            return render_template('register.html',email=email, name=name, alert=render_template('alert.html', errormsg=error))
+            return render_template('register.html', email=email, name=name, alert=render_template('alert.html', errormsg=error))
         else:
             try:
                 create_user(name, email, password)
-                #once user created, log them in directly
+                # Once user created, log them in directly
                 created_user = validate_email(email)
                 proceed_to_login(created_user, True)
                 return redirect(url_for('main'))
@@ -146,17 +164,20 @@ def register_user(name, email, password):
                 dbsession.rollback()
                 return redirect(url_for('createUser'))
 
-#Helper method to show error using alert.html
+
+# Helper method to show error using alert.html
 def show_error(error_string, template_name):
     return render_template(template_name, alert=render_template('alert.html', errormsg=error_string))
 
-#Checking if user exists, returns user object or None
+
+# Checking if user exists, returns user object or None
 def validate_email(email):
     dbsession.commit()
     user = dbsession.query(User).filter_by(email=email).first()
     return user
 
-#Create new user and commit to database
+
+# Create new user and commit to database
 def create_user(name, email, password):
     hPass = hash_str(password)
     todayDate = get_date()
@@ -164,12 +185,14 @@ def create_user(name, email, password):
     dbsession.add(user)
     dbsession.commit()
 
-#Checking if hashed password is the same like the password
+
+# Checking if hashed password is the same like the password
 def check_password_valid(user, password):
     hPass = hash_str(password)
     return user.password == hPass
 
-#Login user with remember param
+
+# Login user with remember param
 def proceed_to_login(user, remember):
     if remember is not None:
         login_user(user, remember=True)
--- main.py
+++ main.py
@@ -1,8 +1,21 @@
-from flask import Flask, render_template, request,jsonify, redirect, url_for, flash, abort, session, get_flashed_messages, send_from_directory
-import httplib2, json, requests
-from flask_login import LoginManager, login_required, login_user, logout_user, current_user
-from logic import SECRET, is_safe_url, process_login, register_user, show_error, get_all_posts, check_city, create_new_post, get_user, get_post_by_user, get_post_byID, delete_post, update_post, getAllPosts,getPostJson, get_user_from_email
-import random, string, os
+from flask import Flask, render_template, request, jsonify,
+            edirect, url_for, flash, abort, session,
+            get_flashed_messages, send_from_directory
+
+from flask_login import LoginManager, login_required,
+            login_user, logout_user, current_user
+
+from logic import SECRET, is_safe_url, process_login,
+            register_user, show_error, get_all_posts,
+            check_city, create_new_post, get_user,
+            get_post_by_user, get_post_byID, delete_post, update_post,
+            getAllPosts, getPostJson, get_user_from_email
+import random
+import string
+import os
+import httplib2
+import json
+import requests
 
 from database_setup import Base, User, Post, Ads, Family, Comment
 from werkzeug.utils import secure_filename
@@ -10,22 +23,25 @@ from werkzeug.utils import secure_filename
 from oauth2client.client import flow_from_clientsecrets
 from oauth2client.client import FlowExchangeError
 
-#securing type of file uploaded
+# Securing type of file uploaded
 UPLOAD_FOLDER = './pictures'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 
 app = Flask(__name__)
 app.secret_key = 'kiasu_secret'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-#securing upload
+
+
+# Securing upload
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-#initializing flask app and login manager
+# Initializing flask app and login manager
 
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
+
 
 @app.route('/logout')
 @login_required
@@ -35,24 +51,28 @@ def logout():
     logout_user()
     return redirect(url_for('main'))
 
-#Routes to homepage
+
+# Routes to homepage
 @app.route('/')
 def main():
     posts = get_all_posts()
     return render_template('home.html', posts=posts)
 
-#Route to About us page
+
+# Route to About us page
 @app.route('/about-us')
 def aboutUs():
     return render_template('about-us.html')
 
-#Route to Contact Us page
+
+# Route to Contact Us page
 @app.route('/contact')
 def contactUs():
     return render_template('contact-us.html')
 
-#Routes to login user
-@app.route('/login', methods=['GET','POST'])
+
+# Routes to login user
+@app.route('/login', methods=['GET', ß'POST'])
 def login():
     if request.method == 'POST':
         email = request.form['email']
@@ -63,11 +83,13 @@ def login():
     else:
         if get_flashed_messages():
             successmsg = get_flashed_messages()[0]
-            return render_template('login.html', alert=render_template('success.html', successmsg=successmsg))
+            return render_template('login.html',
+                alert=render_template('success.html', successmsg=successmsg))
         else:
-             return render_template('login.html')
+            return render_template('login.html')
 
-#Routes to register a new user
+
+# Routes to register a new user
 @app.route('/register', methods=['GET', 'POST'])
 def createUser():
     if request.method == 'POST':
@@ -80,7 +102,9 @@ def createUser():
         if name and email and password and confirmpswd:
             if confirmpswd != password:
                 error = "Verify Password is not the same as Password!"
-                return render_template('register.html', name=name, email=email, alert=render_template('alert.html', errormsg=error))
+                return render_template('register.html',
+                                    name=name, email=email,
+                                    alert=render_template('alert.html', errormsg=error))
             else:
                 return register_user(name, email, password)
         else:
@@ -88,7 +112,8 @@ def createUser():
     else:
         return render_template('register.html')
 
-#user profile page, accessible for logged in user only
+
+# User profile page, accessible for logged in user only
 @app.route('/user/<int:user_id>')
 @login_required
 def userProfile(user_id):
@@ -102,27 +127,33 @@ def userProfile(user_id):
                 flash(render_template('success.html', successmsg=successmsg))
                 return redirect(url_for('main'))
         else:
-            flash(render_template('alert.html', errormsg="Sorry, no user with that ID!"))
+            flash(render_template('alert.html',
+                                    errormsg="Sorry, no user with that ID!"))
             return redirect(url_for('main'))
+
 
 @app.route('/user/<int:user_id>/edit')
 def editUserProfile(user_id):
     return "Edit page for user %s" % user_id
 
-#Route to show how to use the platform
+
+# Route to show how to use the platform
 @app.route('/cara-penggunaan')
 def howToUse():
     return "How to use the platform"
 
-#Routes to create new post, check, edit & delete a single post
+
+# Routes to create new post, check, edit & delete a single post
 @app.route('/pusat-berita-kematian')
 def showAllPosts():
     posts = get_all_posts()
     return render_template('post-listing.html', posts=posts)
 
+
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
 
 @app.route('/newpost', methods=['GET', 'POST'])
 @login_required
@@ -137,7 +168,7 @@ def createNewPost():
             semayam = request.form['semayam']
             beritaduka = request.form['obituary']
 
-            #validating input from user
+            # Validating input from user
             city = request.form['city']
             if city:
                 curCity = check_city(city)
@@ -157,10 +188,14 @@ def createNewPost():
                         pict.save(savedPath)
                         pict_url = url_for('uploaded_file', filename=filename)
                         print "pict url : %s" % pict_url
-                        newPost = create_new_post(name, age, tod, rumahduka, semayam, tos, pict_url, beritaduka, current_user,curCity)
+                        newPost = create_new_post(name, age,
+                                                    tod, rumahduka,
+                                                    semayam, tos,
+                                                    pict_url, beritaduka,
+                                                    current_user,curCity)
 
                         return redirect('post/%s' % newPost.id)
-            #return an error with explaination if form is wrongly submitted
+            # Return an error with explaination if form is wrongly submitted
             else:
                 flash(render_template('alert.html', errormsg="Error in getting City"))
                 return render_template('new-post.html')
@@ -171,6 +206,7 @@ def createNewPost():
     else:
         return render_template('new-post.html')
 
+
 @app.route('/post/<int:post_id>')
 def getSinglePost(post_id):
     post = get_post_byID(post_id)
@@ -178,18 +214,23 @@ def getSinglePost(post_id):
     delete = render_template('delete.html', post_id=post_id)
     if not current_user.is_anonymous:
         if post.user_id == current_user.id:
-            return render_template('detail-post.html', post= post, user = current_user, edit=edit, delete = delete)
+            return render_template('detail-post.html',
+                                    post=post,
+                                    user=current_user,
+                                    edit=edit,
+                                    delete=delete)
         else:
-            return render_template('detail-post.html', post= post)
+            return render_template('detail-post.html', post=post)
     else:
-        return render_template('detail-post.html', post= post)
+        return render_template('detail-post.html', post=post)
+
 
 @app.route('/post/<int:post_id>/edit', methods=['GET', 'POST'])
 @login_required
 def editPost(post_id):
     post = get_post_byID(post_id)
     if request.method == 'POST':
-        #check if the user is currently authorized to edit
+        # Check if the user is currently authorized to edit
         if post.user_id == current_user.id:
             name = request.form['name']
             age = request.form['age']
@@ -199,7 +240,7 @@ def editPost(post_id):
             semayam = request.form['semayam']
             beritaduka = request.form['obituary']
 
-            #validating input from user
+            # Validating input from user
             city = request.form['city']
             if city:
                 curCity = check_city(city)
@@ -219,24 +260,30 @@ def editPost(post_id):
                         pict.save(savedPath)
                         pict_url = url_for('uploaded_file', filename=filename)
                         print "pict url : %s" % pict_url
-                        newPost = update_post(post_id, name, age, tod, rumahduka, semayam, tos, pict_url, beritaduka, current_user,curCity)
+                        newPost = update_post(post_id, name,
+                                                age, tod,
+                                                rumahduka, semayam,
+                                                tos, pict_url,
+                                                beritaduka,
+                                                current_user,curCity)
 
                         return redirect('post/%s' % newPost.id)
-            #return an error with explaination if form is wrongly submitted
+            # Return an error with explaination if form is wrongly submitted
             else:
                 flash(render_template('alert.html', errormsg="Error in getting City"))
                 return render_template('new-post.html')
         else:
-            flash(render_template('alert.html', errormsg ="You can't edit the post!"))
+            flash(render_template('alert.html', errormsg="You can't edit the post!"))
             return redirect('/')
     else:
         if post.user_id == current_user.id:
             return render_template('edit-post.html', post=post)
         else:
-            flash(render_template('alert.html', errormsg ="You can't edit the post!"))
+            flash(render_template('alert.html', errormsg="You can't edit the post!"))
             return redirect('/')
 
-#routes to delete post by id. Only authenticated user can edit his own post
+
+# Routes to delete post by id. Only authenticated user can edit his own post
 @app.route('/post/<int:post_id>/delete', methods=['GET', 'POST'])
 @login_required
 def deletePost(post_id):
@@ -247,31 +294,34 @@ def deletePost(post_id):
             flash(render_template('success.html', successmsg="Post successfully deleted!"))
             return redirect('/')
         else:
-            flash(render_template('alert.html', errormsg ="You can't edit the post!"))
+            flash(render_template('alert.html', errormsg="You can't edit the post!"))
             return redirect('/')
     else:
         if post.user_id == current_user.id:
             return render_template('delete-post.html', post=post)
         else:
-            flash(render_template('alert.html', errormsg ="You can't edit the post!"))
+            flash(render_template('alert.html',
+                                    errormsg="You can't edit the post!"))
             return redirect('/')
 
 
-@app.route('/oauth/<provider>', methods = ['POST'])
+@app.route('/oauth/<provider>', methods=['POST'])
 def loginGoogle(provider):
-    #STEP 1 - Parse the auth code
+    # STEP 1 - Parse the auth code
     print "JSON %s" % request
     auth_code = request.data
     print "Step 1 - Complete, received auth code %s" % auth_code
     if provider == 'google':
-        #STEP 2 - Exchange for a token
+        # STEP 2 - Exchange for a token
         try:
             # Upgrade the authorization code into a credentials object
-            oauth_flow = flow_from_clientsecrets('./static/google-api.json', scope='')
+            oauth_flow = flow_from_clientsecrets('./static/google-api.json',
+                                                    scope='')
             oauth_flow.redirect_uri = 'postmessage'
             credentials = oauth_flow.step2_exchange(auth_code)
         except FlowExchangeError:
-            response = make_response(json.dumps('Failed to upgrade the authorization code.'), 401)
+            response = make_response(json.dumps('Failed to upgrade the authorization code.'),
+                                        401)
             response.headers['Content-Type'] = 'application/json'
             return response
 
@@ -285,33 +335,14 @@ def loginGoogle(provider):
             response = make_response(json.dumps(result.get('error')), 500)
             response.headers['Content-Type'] = 'application/json'
 
-        # # Verify that the access token is used for the intended user.
-        # gplus_id = credentials.id_token['sub']
-        # if result['user_id'] != gplus_id:
-        #     response = make_response(json.dumps("Token's user ID doesn't match given user ID."), 401)
-        #     response.headers['Content-Type'] = 'application/json'
-        #     return response
-
-        # # Verify that the access token is valid for this app.
-        # if result['issued_to'] != CLIENT_ID:
-        #     response = make_response(json.dumps("Token's client ID does not match app's."), 401)
-        #     response.headers['Content-Type'] = 'application/json'
-        #     return response
-
-        # stored_credentials = login_session.get('credentials')
-        # stored_gplus_id = login_session.get('gplus_id')
-        # if stored_credentials is not None and gplus_id == stored_gplus_id:
-        #     response = make_response(json.dumps('Current user is already connected.'), 200)
-        #     response.headers['Content-Type'] = 'application/json'
-        #     return response
         print "Step 2 Complete! Access Token : %s " % credentials.access_token
 
-        #STEP 3 - Find User or make a new one
+        # STEP 3 - Find User or make a new one
 
-        #Get user info
+        # Get user info
         h = httplib2.Http()
-        userinfo_url =  "https://www.googleapis.com/oauth2/v1/userinfo"
-        params = {'access_token': credentials.access_token, 'alt':'json'}
+        userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
+        params = {'access_token': credentials.access_token, 'alt': 'json'}
         answer = requests.get(userinfo_url, params=params)
 
         data = answer.json()
@@ -321,15 +352,12 @@ def loginGoogle(provider):
         email = data['email']
         g_id = data['id']
 
-
-
-        #see if user exists, if it doesn't make a new one
+        # See if user exists, if it doesn't make a new one
         user = get_user_from_email(email)
         if not user:
             register_user(name, email, g_id)
 
         return process_login(email, g_id, True)
-
 
 
 @app.route('/getAllPostJson', methods=['GET', 'POST'])
@@ -353,6 +381,7 @@ def load_user(user_id):
     user = get_user(user_id)
     return user
 
+
 if __name__ == '__main__':
     app.debug = True
-    app.run(host = '0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=8000)
